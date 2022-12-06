9fccae060b6cb035a0e65dd3030b7f16dd325344
Alexey <magnitola1.ru@gmail.com>
2022-12-06 18:48:13 +0300

    222

---
+++ Start2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Start2.Weapons;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Security.Cryptography;
+
+namespace Start2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pistol pistol = new Pistol(20, 5, "BlackMetal", 0.7, false);
+            pistol.Info();
+            pistol.BurstShoot();
+            pistol.Info();
+
+
+
+
+             Random rnd = new Random();
+            machinegun machinegun = new (rnd.Next(0, 100), 5, "Pink", 3.5, true);
+            Random rnd2 = new Random();
+            machinegun machinegun2 = new(rnd.Next(0, 100), 5, "Pink", 3.5, true);
+            Random rnd3 = new Random();
+            machinegun machinegun3 = new(rnd.Next(0, 100), 5, "Pink", 3.5, true);
+
+            machinegun.Info();
+            machinegun.BurstShoot();
+            machinegun.Info();
+
+
+
+            }
+    }
+
+
+}
+
---
+++ Start2/machinegun.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start2.Weapons
+{
+    internal class machinegun
+    {
+
+        public int damage;
+        public int rateOfFire;
+        public string color;
+        public double weight;
+        public int clip;
+        public bool canBurstShooting;
+
+        public machinegun(int damage, int rateOfFire, string color, double weight, bool canBurstShooting)
+        {
+            this.damage = damage;
+            this.rateOfFire = rateOfFire;
+            this.color = color;
+            this.weight = weight;
+            clip = 35;
+            this.canBurstShooting = canBurstShooting;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Weapon - machinegun. Damage {damage} Clip: {clip}");
+        }
+        public void Shoot()
+        {
+            Console.WriteLine($"Tra-ta-ta! Damage {damage}");
+            clip--;
+        }
+        public void Shoot2()
+        {
+            Console.WriteLine($"Tra-ta-ta! Damage {damage}");
+            clip--;
+        }
+        public void Shoot3()
+        {
+            Console.WriteLine($"Tra-ta-ta! Damage {damage}");
+            clip--;
+        }
+
+        public void BurstShoot()
+        {
+            if (canBurstShooting)
+            {
+                Shoot();
+                Shoot2();
+                Shoot3();
+            }
+            else
+                Shoot();
+        }
+    }
+}
